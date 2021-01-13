27824c349990baa90cde0a27a9ac3a4ddc889ef6
Flavio Samu <fsamu40@gmail.com>
2021-01-13 08:38:20 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/fsamu/course/config/TestConfig.java
+++ src/main/java/com/fsamu/course/config/TestConfig.java
@@ -48,6 +48,14 @@ public class TestConfig implements CommandLineRunner {
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
 		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
@@ -59,7 +67,6 @@ public class TestConfig implements CommandLineRunner {
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITTING_PAYMENT, u1); 
 				
 		userRepository.saveAll(Arrays.asList(u1, u2));
-		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
 }
