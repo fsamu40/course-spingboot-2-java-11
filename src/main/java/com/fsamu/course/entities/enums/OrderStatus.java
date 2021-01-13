7bbe3ed0d9e6faa2142c19699b15f188fdf8f3ca
Flavio Samu <fsamu40@gmail.com>
2021-01-13 04:45:51 -0300

    OrderStatus enum

---
+++ src/main/java/com/fsamu/course/entities/enums/OrderStatus.java
@@ -0,0 +1,25 @@
+package com.fsamu.course.entities.enums;
+
+public enum OrderStatus {
+
+	WAITTING_PAYMENT(1), PAID(2), SHIPPED(3), DELIVERED(4), CANCELED(5);
+
+	private int code;
+
+	private OrderStatus(int code) {
+		this.code = code;
+	}
+
+	public int getCOde() {
+		return code;
+	}
+
+	public static OrderStatus valueOf(int code) {
+		for (OrderStatus value : OrderStatus.values()) {
+			if (value.getCOde() == code) {
+				return value;
+			}
+		}
+		throw new IllegalArgumentException("Invalid OrderStatus code");
+	}
+}
